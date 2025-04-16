300388509b5a061a877b0f026adfa41c86f33caa
fefefe12 <cookiesxd419@gmail.com>
2025-04-17 03:16:55 +0530

    Update whois.py

--- AvishaRobot/modules/whois.py
+++ AvishaRobot/modules/whois.py
@@ -8,7 +8,7 @@ from AvishaRobot import pbot
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/avishaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/AlisaMusicRobot?startgroup=true"),
     ],
 ]
 
@@ -31,7 +31,7 @@ infotext = (
     "● ʟᴀsᴛ ɴᴀᴍᴇ ➥ `{last_name}`\n"
     "● ᴜsᴇʀɴᴀᴍᴇ ➥ `@{username}`\n"
     "● ʟᴀsᴛ sᴇᴇɴ ➥ `{last_online}`\n\n"
-    "❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐](https://t.me/avishaxbot)"
+    "❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ʟ ɪ s ʜ ᴀ ࿐](https://t.me/AlisaMusicRobot)"
 )
 
 
