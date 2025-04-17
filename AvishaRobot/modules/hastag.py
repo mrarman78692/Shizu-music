141907b3cdf5a81d8ba051ccc18936ab4cf23cb9
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 01:55:48 +0530

    Update hastag.py

--- AvishaRobot/modules/hastag.py
+++ AvishaRobot/modules/hastag.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Avishaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/AlisaMusicRobot?startgroup=true"),
     ],
 ]
 
