72f01118fe35a5eae86d272c275c7ab7512f0a85
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 17:35:56 +0530

    Update blacklist_stickers.py

--- AvishaRobot/modules/blacklist_stickers.py
+++ AvishaRobot/modules/blacklist_stickers.py
@@ -499,7 +499,7 @@ __help__ = """
  ⬤ <sticker link > ᴄᴀɴ ʙᴇ `https://t.me/addstickers//<sticker>` ᴏʀ ᴊᴜsᴛ `<sticker>` ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ sᴛɪᴄᴋᴇʀ ᴍᴇssᴀɢᴇ
 """
 
-__mod_name__ = "sᴛɪᴄᴋᴇʀ"
+__mod_name__ = "˹ sᴛɪᴄᴋᴇʀ ˼"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True, run_async=True
