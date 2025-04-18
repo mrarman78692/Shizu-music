a4a4ae0d1d7643e2cc1fd591516b38b8350aac11
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 07:18:10 +0530

    Update qr_code.py

--- AvishaRobot/modules/qr_code.py
+++ AvishaRobot/modules/qr_code.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/avishaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/AlisaMusicRobot?startgroup=true"),
     ],
 ]
 
@@ -42,7 +42,7 @@ def qr_handler(client, message: Message):
     if len(command_text) > 1:
         input_text = " ".join(command_text[1:])
         qr_image = generate_qr_code(input_text)
-        message.reply_photo(qr_image, caption="❖ ǫʀ ɢᴇɴ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐ ", reply_markup=InlineKeyboardMarkup(EVAA),)
+        message.reply_photo(qr_image, caption="❖ ǫʀ ɢᴇɴ ʙʏ ➥ ๛ᴀ ʟ ɪ s ʜ ᴀ ࿐ ", reply_markup=InlineKeyboardMarkup(EVAA),)
 
     else:
         message.reply_text("⬤ Please provide the text for the QR code after the command. Example usage ➥ /qr text")
