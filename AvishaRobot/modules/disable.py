26540ff911847ddaa310d226e8a079a46f279572
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 19:06:09 +0530

    Update disable.py

--- AvishaRobot/modules/disable.py
+++ AvishaRobot/modules/disable.py
@@ -347,7 +347,7 @@ if is_module_loaded(FILENAME):
     ⬤ /listcmds* ➥* ʟɪsᴛ ᴀʟʟ ᴘᴏssɪʙʟᴇ ᴛᴏɢɢʟᴇᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs.
     """
 
-    __mod_name__ = "ᴅɪsᴀʙʟᴇ"
+    __mod_name__ = "˹ ᴅɪsᴀʙʟᴇ ˼"
 
 else:
     DisableAbleCommandHandler = CommandHandler
