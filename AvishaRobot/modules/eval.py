323b32ed9427225ccb50a74d60c2c4b53b10b188
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 19:16:30 +0530

    Update eval.py

--- AvishaRobot/modules/eval.py
+++ AvishaRobot/modules/eval.py
@@ -130,7 +130,7 @@ dispatcher.add_handler(EVAL_HANDLER)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
-__mod_name__ = "ᴇᴠᴀʟ"
+__mod_name__ = "˹ ᴇᴠᴀʟ ˼"
 
 __help__ = f"""
 
