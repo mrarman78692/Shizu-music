96df0dafcb38d85d275872ff9422f6a5a3aa1968
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 21:07:06 +0530

    Update cust_filters.py

--- AvishaRobot/modules/cust_filters.py
+++ AvishaRobot/modules/cust_filters.py
@@ -625,7 +625,7 @@ __help__ = """
 
 """
 
-__mod_name__ = "Fɪʟᴛᴇʀs"
+__mod_name__ = "˹ ғɪʟᴛᴇʀs ˼"
 
 FILTER_HANDLER = CommandHandler("filter", filters, run_async=True)
 STOP_HANDLER = CommandHandler("stop", stop_filter, run_async=True)
