e5db3399b584fcfded47f1b3af4c5c72c8c3b69f
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 18:42:39 +0530

    Update fun.py

--- AvishaRobot/modules/fun.py
+++ AvishaRobot/modules/fun.py
@@ -257,7 +257,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "ғᴜɴ"
+__mod_name__ = "˹ ғᴜɴ ˼"
 
 __command_list__ = [
     "runs",
