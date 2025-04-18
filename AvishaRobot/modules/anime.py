3bd7ef8fae0441786fbb0045b82310f802bd7444
fefefe12 <cookiesxd419@gmail.com>
2025-04-18 18:46:07 +0530

    Update anime.py

--- AvishaRobot/modules/anime.py
+++ AvishaRobot/modules/anime.py
@@ -139,7 +139,7 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "ᴀ-ǫᴜᴏᴛᴇ"
+__mod_name__ = "˹ ᴀ-ǫᴜᴏᴛᴇ ˼"
 
 __help__ = """
 
